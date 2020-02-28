e0619909adae29260143db6d42ee0de90f18f2ad
hiphop5782 <zdlsz@hanmail.net>
2020-02-29 00:56:20 +0900

    예제 추가 및 js, css 개선

--- meteor/hakademy-meteor.js
+++ meteor/hakademy-meteor.js
@@ -7,6 +7,20 @@
             throw "Meteor : 적용 대상이 없습니다";
 
         var defaultOptions = {
+            staticStarCount:50,
+            staticStarStyle:"colorful",//null, colorful
+            staticStarColorList:[
+                "#f5d300",
+                "#09fbd3",
+                "#08f7fe",
+                "#b4dafc",
+            	"#cca4fd",
+                "#ffff8f",
+                "#aafec6",
+            	"#78fdfa",
+            	"#34fd50",
+            	"#f4f231"
+            ],
             starCount:30,//meteor count
             starMinDelay:0,
             starMaxDelay:5,
@@ -107,19 +121,48 @@
             var st = window.getComputedStyle(this.el[k]);
             var w = Math.max(this.el[k].offsetWidth, parseInt(st.width));
             var h = Math.max(this.el[k].offsetHeight, parseInt(st.height));
+
+            //static star
+            for(var i=0; i < this.options.staticStarCount; i++){
+                var static_star = document.createElement("div");
+
+                var left = Math.floor(Math.random() * w);
+                var top = Math.floor(Math.random() * h);
+                var delay = Math.floor(Math.random() * 5);
+                var duration = Math.floor(Math.random() * 20) + 10;
+
+                static_star.classList.add("static-star");
+                static_star.style.top = top + "px";
+                static_star.style.left = left + "px";
+                static_star.style.animationDelay = delay+"s";
+                static_star.style.animationDuration = duration+"s";
+
+                if(this.options.staticStarStyle === "colorful"){
+                    var index = Math.floor(Math.random() * this.options.staticStarColorList.length);
+                    static_star.style.backgroundColor = this.options.staticStarColorList[index];
+                }
+
+                this.el[k].appendChild(static_star);
+            }
+
+            //dynamic star
             for(var i=0; i < this.options.starCount; i++){
+                var star = document.createElement("div");
+
                 var top = parseInt(Math.random() * h - h * 20 / 100);
                 var left = parseInt(Math.random() * w + w * 20 / 100);
                 var delay = Math.random() * (this.options.starMaxDelay - this.options.starMinDelay + 1) + this.options.starMinDelay;
                 var duration = Math.random() * (this.options.starMaxDuration - this.options.starMinDuration + 1) + this.options.starMinDuration;
-                var star = document.createElement("div");
+
                 star.classList.add("star");
                 star.style.top = top+"px";
                 star.style.left = left+"px";
                 star.style.animationDelay = delay+"s";
                 star.style.animationDuration = duration+"s";
+
                 var index = Math.floor(Math.random() * this.options.starColorList.length);
                 star.style.backgroundColor = this.options.starColorList[index];
+
                 this.el[k].appendChild(star);
             }
         }
