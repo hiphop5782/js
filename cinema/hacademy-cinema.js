ad306c556a4b3068b889aed880e552a5d203f39b
hiphop5782 <zdlsz@hanmail.net>
2020-08-29 17:23:01 +0900

    cinema update

--- cinema/hacademy-cinema.js
+++ cinema/hacademy-cinema.js
@@ -2,36 +2,20 @@
     w.Hacademy = w.Hacademy || {};
     w.Hacademy.util = w.Hacademy.util || {};
     
-    //좌석 선택 이벤트 핸들러
-    function SeatLeftClickHandler(){
-        var checkbox = this.querySelector("input[type=checkbox]");
-        if(checkbox.checked){
-            checkbox.checked = false;
-            this.classList.remove("active");
-        }
-        else{
-            checkbox.checked = true;
-            this.classList.add("active");
-        }
-    }
-    function SeatRightClickHandler(e){
-        e.preventDefault();
-        switch(e.target.dataset.direction){
-            case 'up': e.target.dataset.direction = 'right'; break;
-            case 'right': e.target.dataset.direction = 'down'; break;
-            case 'down': e.target.dataset.direction = 'left'; break;
-            case 'left': e.target.dataset.direction = 'up'; break;
-        }
-    }
-
     w.Hacademy.Reservation = function(area, callback){
         if(!area) 
             throw "영역이 정의되지 않았습니다";
 
         if(typeof area === 'string')
             area = document.querySelector(area);
+    
+        //영역 우클릭 방지
+        area.addEventListener("contextmenu", function(e){
+            e.preventDefault();
+        });
 
         var seatArea = area.querySelector(".cinema-seat-area");
+        console.log(seatArea);
         if(!seatArea){
             throw "좌석 영역이 지정되지 않았습니다(class='cinema-seat-area')";
         }
@@ -51,17 +35,22 @@
         //좌석 크기 계산(좌석은 무조건 정사각형)
         var unitSize = parseInt(seatAreaWidth / colsize);
 
+        //좌석 관리 모드
+        //data-mode="manager" : 관리자 모드(좌석 선택 후 좌석 삭제 및 공간 병합 가능)
+        //data-mode="client" : 사용자 모드(좌석 선택만 가능) - 기본값
+        var mode = seatArea.dataset.mode || "client";
+
         //좌석 배치 방법은 2가지
-        //data-mode="auto" : 자동으로 빈좌석을 채움(기본값)
-        //data-mode="manual" : 수동으로 좌석을 채움
-        var mode = seatArea.dataset.mode || 'auto';
+        //data-fill="auto" : 자동으로 빈좌석을 채움(기본값)
+        //data-fill="manual" : 수동으로 좌석을 채움
+        var fill = seatArea.dataset.fill || 'auto';
 
         //전송 이름(없으면 seat으로 설정)
         var sendName = area.dataset.name || 'seat';
-        
-        //배치방향(없으면 up으로 설정)
-        var direction = area.dataset.direction || 'up';
 
+        //좌석 표시 여부(없으면 hidden)
+        var seatNoVisible = seatArea.dataset.seatno || "hidden";
+        
         this.options = {
             area:area,
             seatArea:seatArea,
@@ -70,36 +59,106 @@
             seatAreaWidth:seatAreaWidth,
             unitSize:unitSize,
             mode:mode,
+            fill:fill,
             sendName:sendName,
-            direction:direction
+            seatNoVisible:seatNoVisible
         };
 
-        if(mode === 'auto'){
+        if(fill === 'auto'){
             this.automaticFillProcess();
         }
         else{
             this.manualFillProcess();
         }
+        
+        if(mode === "manager"){
+            this.addManagerEventListener();
+        }
 
         var app = this;
+
         w.addEventListener("resize", function(){
             app.resize();
         });
 
         app.resize();
+
+        if(callback)
+            callback();
+    };
+
+    w.Hacademy.Reservation.prototype.addManagerEventListener = function(){
+        var app = this;
+        var area = this.options.area;
+        area.setAttribute("tabindex", "0");
+        area.addEventListener("mouseenter", function(e){
+            e.target.focus();
+        });
+        area.addEventListener("keydown", function(e){
+            e.preventDefault();
+            switch(e.key.toLowerCase()){
+                case "f2": app.mergeSpace(); break;
+                case "f3": app.setDisable(); break;
+                case "delete": app.deleteSpace(); break;
+            }
+        });
+        var seatArea = this.options.seatArea;
+        var seatList = seatArea.querySelectorAll(".cinema-seat");
+        for(var i=0; i < seatList.length; i++){
+            seatList[i].addEventListener("contextmenu", function(e){
+                e.preventDefault();
+                app.changeUnit(this, "direction");
+            });
+        }
+    };
+
+    w.Hacademy.Reservation.prototype.addChangeListener = function(listener){
+        if(typeof listener !== "function") return;
+        this.changeListener = listener;
+    };
+
+    w.Hacademy.Reservation.prototype.getQueryString = function(){
+        var checkedList;
+        if(this.options.mode === "manager")
+            checkedList = this.options.seatArea.querySelectorAll(".cinema-seat:not(.empty),.cinema-space");
+        else if(this.options.mode === "client")
+            checkedList = this.options.seatArea.querySelectorAll(".cinema-seat.active");
+        if(!checkedList || checkedList.length == 0){
+            return;
+        }
+
+        var params = new URLSearchParams();
+        for(var i=0; i<checkedList.length; i++){
+            var unit = checkedList[i].querySelector("input[type=hidden]");
+            params.append(unit.name, unit.value);
+        }
+        return params.toString();
+    };
+
+    w.Hacademy.Reservation.prototype.setDisable = function(){
+        var activeSeatList = this.options.seatArea.querySelectorAll(".cinema-seat.active");
+        for(var i=0; i<activeSeatList.length; i++){
+            this.changeUnit(activeSeatList[i], "disabled");
+        }
     };
 
     w.Hacademy.Reservation.prototype.automaticFillProcess = function(){
-        for(var i=0; i < this.options.rowsize; i++){
-            for(var j=0; j < this.options.colsize; j++){
-                var value = i + "-" + j + "-" + this.options.direction;
+        for(var i=1; i <= this.options.rowsize; i++){
+            for(var j=1; j <= this.options.colsize; j++){
                 var seat = this.createUnit("normal");
-                seat.dataset.direction = this.options.direction;
-                seat.setValue(value);
+                seat.dataset.direction = "up";
+                seat.dataset.row = i;
+                seat.dataset.col = j;
+                if(seat.setSeatNumber){
+                    seat.setSeatNumber(i+"-"+j);
+                }
                 this.options.seatArea.appendChild(seat);
             }
         }
+
+        this.refreshValue();
     };
+
     w.Hacademy.Reservation.prototype.manualFillProcess = function(){
         var rowsize = this.options.rowsize;
         var colsize = this.options.colsize;
@@ -110,13 +169,16 @@
             for(var j=1; j <= colsize; j++){
                 var findElement = cloneSeatArea.querySelector(".cinema-seat[data-row='"+i+"'][data-col='"+j+"']");
                 if(findElement){
-                    var value = i + "-" + j;
                     var state = findElement.dataset.state || "normal";
                     var seat = this.createUnit(state);
-                    if(seat.setValue){
-                        seat.setValue(value);
-                    }
+                    seat.dataset.row = i;
+                    seat.dataset.col = j;
+                    seat.dataset.direction = findElement.dataset.direction || "up";
+                    var value = i + "-" + j;
                     seatArea.appendChild(seat);
+                    if(seat.setSeatNumber){
+                        seat.setSeatNumber(i+"-"+j);
+                    }
                 }
                 else {
                     var seat = this.createUnit("empty");
@@ -124,8 +186,11 @@
                 }
             }
         }
+        this.refreshSpace();
     };
+
     w.Hacademy.Reservation.prototype.createUnit = function(v){
+        var app = this;
         if(v === 'empty'){
             var seat = this.createUnit();
             seat.classList.add("empty");
@@ -133,27 +198,19 @@
         }
         else if(v === 'active'){
             var seat = this.createUnit();
-            var checkbox = this.createCheckbox(true);
-            seat.appendChild(checkbox);
             seat.classList.add("active");
-            seat.addEventListener("click", SeatLeftClickHandler);
-            seat.addEventListener("contextmenu", SeatRightClickHandler);
-            seat.setValue = function(value){
-                var checkbox = this.querySelector("input[type=checkbox]");
-                checkbox.value = value;
-            };
+            seat.addEventListener("click", function(e){
+                var isActive = this.classList.contains("active");
+                app.changeUnit(this, isActive?"normal":"active");
+            });
             return seat;
         }
         else if(v === 'normal'){
             var seat = this.createUnit();
-            var checkbox = this.createCheckbox();
-            seat.appendChild(checkbox);
-            seat.addEventListener("click", SeatLeftClickHandler);
-            seat.addEventListener("contextmenu", SeatRightClickHandler);
-            seat.setValue = function(value){
-                var checkbox = this.querySelector("input[type=checkbox]");
-                checkbox.value = value;
-            };
+            seat.addEventListener("click", function(e){
+                var isActive = this.classList.contains("active");
+                app.changeUnit(this, isActive?"normal":"active");
+            });
             return seat;
         }
         else if(v === 'disabled'){
@@ -161,23 +218,199 @@
             seat.classList.add("disabled");
             return seat;
         }
-        else{
+        else if(v === "space"){
             var seat = document.createElement("div");
-            seat.classList.add("cinema-seat");
+            seat.classList.add("cinema-space");
             seat.style.width = this.options.unitSize+"px";
             seat.style.height = this.options.unitSize+"px";
             return seat;
         }
+        else{
+            var seat = document.createElement("div");
+            seat.appendChild(this.createHiddenInput());
+            seat.classList.add("cinema-seat");
+            seat.style.width = this.options.unitSize+"px";
+            seat.style.height = this.options.unitSize+"px";
+            if(this.options.seatNoVisible === "visible"){
+                var span = document.createElement("span");
+                seat.setSeatNumber = function(v){
+                    span.textContent = v;
+                }
+                span.addEventListener("contextmenu", function(e){
+                    e.preventDefault();
+                    e.stopPropagation();
+                    seat.dispatchEvent(new Event("contextmenu"));
+                });
+                seat.appendChild(span);
+            }
+            return seat;
+        }
     };
 
-    w.Hacademy.Reservation.prototype.createCheckbox = function(check){
-        var checkbox = document.createElement("input");
-        checkbox.setAttribute("type", "checkbox");
-        checkbox.setAttribute("name", this.options.sendName);
-        checkbox.style.display = "none";
-        checkbox.checked = !!check;
-        return checkbox;
+    w.Hacademy.Reservation.prototype.createHiddenInput = function(){
+        var hidden = document.createElement("input");
+        hidden.setAttribute("type", "hidden");
+        hidden.setAttribute("name", this.options.sendName);
+        return hidden;
     };
+
+    //영역 삭제 메소드
+    w.Hacademy.Reservation.prototype.deleteSpace = function(){
+        var checkspace = this.options.seatArea.querySelectorAll(".cinema-seat.active");
+        for(var i = 0; i < checkspace.length; i++){
+            this.changeUnit(checkspace[i], "empty");
+        }
+    };
+
+    //영역 병합 메소드
+    w.Hacademy.Reservation.prototype.mergeSpace = function(){
+        var checkspace = this.options.seatArea.querySelectorAll(".cinema-seat.active");
+        for(var i = 0; i < checkspace.length; i++){
+            this.changeUnit(checkspace[i], "space");
+        }
+
+        //좌석 변환 후 space 재정비
+        this.refreshSpace();
+    };
+
+    w.Hacademy.Reservation.prototype.refreshSpace = function(){
+        var checkspace = this.options.seatArea.querySelectorAll(".cinema-space");
+
+        for(var i = 0; i < checkspace.length; i++){
+            this.calculateMergeSpace(checkspace[i]);    
+        }
+    };
+
+    //상하좌우 확인하여 space 정리
+    w.Hacademy.Reservation.prototype.calculateMergeSpace = function(unit){
+        var row = parseInt(unit.dataset.row);
+        var col = parseInt(unit.dataset.col);
+
+        //top
+        if(row > 0){
+            var tag = this.options.seatArea.querySelector(".cinema-space[data-row='"+(row-1)+"'][data-col='"+(col)+"']");
+            if(tag){
+                unit.classList.add("top");
+                if(!tag.classList.contains("bottom"))
+                    tag.classList.add("bottom");
+            }
+        }
+
+        //bottom
+        if(row < this.options.rowsize){
+            var tag = this.options.seatArea.querySelector(".cinema-space[data-row='"+(row+1)+"'][data-col='"+(col)+"']");
+            if(tag){
+                unit.classList.add("bottom");
+                if(!tag.classList.contains("top"))
+                    tag.classList.add("top");
+            }
+        }
+
+        //left
+        if(col > 0){
+            var tag = this.options.seatArea.querySelector(".cinema-space[data-row='"+(row)+"'][data-col='"+(col-1)+"']");
+            if(tag){
+                unit.classList.add("left");
+                if(!tag.classList.contains("right"))
+                    tag.classList.add("right");
+            }
+        }
+
+        //right
+        if(col < this.options.colsize){
+            var tag = this.options.seatArea.querySelector(".cinema-space[data-row='"+(row)+"'][data-col='"+(col+1)+"']");
+            if(tag){
+                unit.classList.add("right");
+                if(!tag.classList.contains("left"))
+                    tag.classList.add("left");
+            }
+        }
+    }
+
+    //의자와 영역을 교체하는 메소드
+    w.Hacademy.Reservation.prototype.changeUnit = function(unit, mode){
+        var app = this;
+
+        if(mode === "space"){
+            unit.classList.remove("active", "empty", "disabled", "cinema-seat", "cinema-space");
+            unit.removeChild(unit.querySelector("span"));
+            unit.classList.add("cinema-space");
+            unit.removeAttribute("data-direction");
+            unit.dataset.state = mode;
+            var newUnit = unit.cloneNode(true);
+            unit.parentNode.replaceChild(newUnit, unit);
+        }
+        else if(mode === "empty"){
+            unit.classList.remove("active", "empty", "disabled", "cinema-seat", "cinema-space");
+            unit.textContent = "";
+            unit.classList.add("cinema-seat", "empty");
+            unit.removeAttribute("data-direction");
+            unit.dataset.state = mode;
+            var newUnit = unit.cloneNode(true);
+            unit.parentNode.replaceChild(newUnit, unit);
+        }
+        else if(mode === "disabled"){
+            unit.classList.remove("active", "empty", "disabled", "cinema-seat", "cinema-space");
+            //unit = unit.cloneNode(true);
+            unit.classList.add("cinema-seat", "disabled");
+            unit.dataset.state = mode;
+            var newUnit = unit.cloneNode(true);
+            newUnit.addEventListener("contextmenu", function(e){
+                e.preventDefault();
+                app.changeUnit("direction");
+            });
+            unit.parentNode.replaceChild(newUnit, unit);
+        }
+        else if(mode === "active"){
+            unit.classList.remove("active", "empty", "disabled", "cinema-seat", "cinema-space");
+            unit.classList.add("cinema-seat", "active");
+            unit.dataset.state = mode;
+        }
+        else if(mode === "normal"){
+            unit.classList.remove("active", "empty", "disabled", "cinema-seat", "cinema-space");
+            unit.classList.add("cinema-seat");
+            unit.dataset.state = mode;
+        }
+        else if(mode === "direction"){
+            switch(unit.dataset.direction){
+                case 'up': unit.dataset.direction = 'right'; break;
+                case 'right': unit.dataset.direction = 'down'; break;
+                case 'down': unit.dataset.direction = 'left'; break;
+                case 'left': unit.dataset.direction = 'up'; break;
+            }
+        }
+
+        //값 변경에 따른 갱신
+        app.refreshValue();
+
+        if(app.changeListener){
+            app.changeListener.call(app, unit);
+        }
+    }
+
+    w.Hacademy.Reservation.prototype.refreshValue = function(){
+        var list = this.options.seatArea.querySelectorAll(".cinema-seat, .cinema-space");
+        for(var i=0; i < list.length; i++){
+            this.setValue(list[i]);
+        }
+    };
+    w.Hacademy.Reservation.prototype.setValue = function(unit){
+        if(unit.classList.contains("empty")) return;
+
+        var seperator = "-";
+
+        var value = "";
+        value += unit.dataset.row;
+        value += seperator;
+        value += unit.dataset.col;
+        if(this.options.mode === "manager"){
+            value += seperator;
+            value += unit.dataset.state || "normal";
+            value += seperator;
+            value += unit.dataset.direction || "up";
+        }
+        unit.querySelector("input").value = value;
+    }
 
     //크기 재조정
     w.Hacademy.Reservation.prototype.resize = function(){
@@ -189,6 +422,12 @@
             seatList[i].style.width = this.options.unitSize + "px";
             seatList[i].style.height = this.options.unitSize + "px";
         }            
+
+        var spaceList = this.options.seatArea.querySelectorAll(".cinema-space");
+        for(var i=0; i < spaceList.length; i++){
+            spaceList[i].style.width = this.options.unitSize + "px";
+            spaceList[i].style.height = this.options.unitSize + "px";
+        }
     };
 
     function calculateInnerWidth(tag){
