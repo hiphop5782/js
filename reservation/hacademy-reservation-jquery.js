7e711a2a27a3f6717ea3785a8c3f54a5cb6bdc75
hiphop5782 <zdlsz@hanmail.net>
2022-06-04 10:45:37 +0900

    구현중

--- reservation/hacademy-reservation-jquery.js
+++ reservation/hacademy-reservation-jquery.js
@@ -19,40 +19,76 @@
         const image = {
             empty:"./image/seat-empty.png",
             active:"./image/seat-active.png",
-            disabled:"./image/seat-disable.png"
+            disabled:"./image/seat-disabled.png"
         };
 
         return this.each(function(){
-            var tag = this;
-            const state = {
-                mode:$(this).data("mode") || "empty",
-                get mode(){
-                    return this.mode;
-                },
-                set mode(value){
-                    this.mode = mode;
-                    $(tag).css("background-image", "url("+image[this.mode]+")");
+            var root = this;
+            
+            const inputEventListener = function(){
+                if(!state.enable) return;
+
+                if($(this).prop("checked")){
+                    state.mode = "active";
+                }
+                else {
+                    state.mode = "empty";
                 }
             };
 
+            
             $(this).css("display", "block");
             $(this).css("width", settings.size);
             $(this).css("height", settings.size);
             $(this).css("background-size", "100% 100%");
+            
+            const checkbox = $("<input>").attr("type", "checkbox").attr("name", "seat")//.hide();
+            checkbox.on("input", inputEventListener);
+            $(this).append(checkbox);
+            
+            const state = {
+                get mode(){
+                    return this._mode;
+                },
+                set mode(value){
+                    this._mode = value;
+                    $(root).css("background-image", "url("+image[this._mode]+")");
+                    switch(this._mode){
+                        case "empty":
+                        case "active":
+                            checkbox.prop("disabled", false);
+                            break;
+                        case "disabled":
+                            checkbox.prop("disabled", true);
+                            break;
+                    }
+                },
+                get enable(){
+                    switch(this._mode){
+                        case "empty": return true;
+                        case "active": return true;
+                        case "disabled": return false;
+                    }
+                },
+                set direction(value){
+                    this._direction = value;
+                    switch(this._direction) {
+                        case "up":
+                            $(root).css("transform", "rotate(0deg)"); break;
+                        case "left":
+                            $(root).css("transform", "rotate(270deg)"); break;
+                        case "right":
+                            $(root).css("transform", "rotate(90deg)"); break;
+                        case "down":
+                            $(root).css("transform", "rotate(180deg)"); break;
+                    }
+                }
+            };
 
-            this.checkbox = $("<input>").attr("type", "checkbox")
-                                                        .attr("name", "seat")
-                                                        //.css("display", "none")
-                                                        ;
-            this.checkbox.on("input", function(){
-                if($(this).is(":checked")){
-                    state.mode = "active";
-                }
-                else {
-                    state.mode = 
-                }
-            });                                                        
-            $(this).append(this.checkbox);
+            state.mode = $(this).data("mode") || "empty";
+            state.direction = $(this).data("direction") || "up";
+            $(this).removeData("mode");
+            $(this).removeData("direction");
         });
     };
 
